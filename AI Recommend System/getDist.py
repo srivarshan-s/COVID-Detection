a66673a17f1ba8283a8dce9a0d063545c97fb98f
srivarshan-s <srivarshan-s@users.noreply.github.com>
2021-03-27 17:52:56 +0530

    Improved speed by 2x times

--- AI Recommend System/getDist.py
+++ AI Recommend System/getDist.py
@@ -2,19 +2,13 @@ from geopy import Nominatim
 import haversine as hs
 
 
-def getLatitude(place):
+def getCoords(place):
     g = Nominatim(user_agent='myGeocoder', timeout=100)
     location = g.geocode(place)
-    return location.latitude
-
-
-def getLongitude(place):
-    g = Nominatim(user_agent='myGeocoder', timeout=100)
-    location = g.geocode(place)
-    return location.longitude
+    return (location.latitude, location.longitude)
 
 
 def getDistance(place1, place2):
-    place1Coords = (getLatitude(place1), getLongitude(place1))
-    place2Coords = (getLatitude(place2), getLongitude(place2))
+    place1Coords = getCoords(place1)
+    place2Coords = getCoords(place2)
     return hs.haversine(place1Coords, place2Coords)
